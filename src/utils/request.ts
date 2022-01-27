b59f7c8c1f8c21bd3196e5e9ee82f170f1460d8b
Jamal Gardiner <jgardiner@neat.com>
2022-01-27 11:16:57 -0500

    feat: better type checking for environment variables in requests

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,11 +4,12 @@ import { rapidApiHost, rapidApiBaseUrl } from '../constants'
 const instance = axios.create()
 
 const nbaApiRequest = async ({ ...rest }) => {
-  console.log(process.env)
-  const hello = process.env
-  console.log(hello)
-  if (typeof process.env.NBA_RAPID_API_KEY !== 'string' || typeof process.env.REACT_APP_NBA_RAPID_API_KEY !== 'string') {
-    throw new Error('Missing environment variable NBA_RAPID_API_KEY')
+  let key
+  if (process.env.NBA_RAPID_API_KEY === undefined) {
+    key = process.env.REACT_APP_NBA_RAPID_API
+  }
+  if (key === undefined) {
+    throw new Error('Missing environment variable REACT_APP_NBA_RAPID_API_KEY')
   }
 
   return instance({
@@ -16,7 +17,7 @@ const nbaApiRequest = async ({ ...rest }) => {
     baseURL: rapidApiBaseUrl,
     headers: {
       'x-rapidapi-host': rapidApiHost,
-      'x-rapidapi-key': (process.env.NBA_RAPID_API_KEY || process.env.REACT_APP_NBA_RAPID_API_KEY),
+      'x-rapidapi-key': key,
     },
     ...rest,
   })
