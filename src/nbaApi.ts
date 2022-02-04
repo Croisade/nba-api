148af85c73581b50f5c0864bc1e3661eeef939dd
Jamal Gardiner <jgardiner@neat.com>
2022-02-04 12:48:12 -0500

    feat: enums for static typings . Include leagues in teamsData

--- src/nbaApi.ts
+++ src/nbaApi.ts
@@ -6,6 +6,16 @@ export enum Conference {
   west = 'west',
 }
 
+export enum Year {
+  twentyFifteen = '2015',
+  twentySixteen = '2016',
+  twentySeventeen = '2017',
+  twentyEighteen = '2018',
+  twentyNineteen = '2019',
+  twentyTwenty = '2020',
+  twentyTwentyOne = '2021',
+}
+
 export enum Division {
   Southeast = 'Southeast',
   Central = 'Central',
@@ -22,6 +32,170 @@ export enum Leagues {
   vegas = 'vegas',
   utah = 'utah',
   orlando = 'orlando',
+}
+
+export enum NickName {
+  grizzlies = 'Grizzlies',
+  lakers = 'Lakers',
+  mavericks = 'Mavericks',
+  nuggets = 'Nuggets',
+  warriors = 'Warriors',
+  rockets = 'Rockets',
+  clippers = 'Clippers',
+  timberwolves = 'Timberwolves',
+  pelicans = 'Pelicans',
+  thunder = 'Thunder',
+  suns = 'Suns',
+  trailBlazers = 'Trail Blazers',
+  kings = 'Kings',
+  spurs = 'Spurs',
+  jazz = 'Jazz',
+  utb = 'Jazz',
+  utw = 'Jazz',
+  lbn = 'Team Lebron',
+  hawks = 'Hawks',
+  celtics = 'Celtics',
+  nets = 'Nets',
+  hornets = 'Hornets',
+  bulls = 'Bulls',
+  cavaliers = 'Cavaliers',
+  pistons = 'Pistons',
+  pacers = 'Pacers',
+  heat = 'Heat',
+  bucks = 'Bucks',
+  knicks = 'Knicks',
+  magic = 'Magic',
+  sixers = '76ers',
+  durant = 'Team Durant',
+  wilbon = 'Team Wilbon',
+  stephenA = 'Team Stephen A',
+  raptors = 'Raptors',
+  usa = 'USA',
+  wizards = 'Wizards',
+  world = ' World',
+}
+
+export enum ShortName {
+  suns = 'PHX',
+  grizzlies = 'MEM',
+  lakers = 'LAL',
+  mavericks = 'DAL',
+  nuggets = 'DEN',
+  warriors = 'GSW',
+  rockets = 'HOU',
+  clippers = 'LAC',
+  timberwolves = 'MIN',
+  pelicans = 'NOP',
+  thunder = 'OKC',
+  trailBlazers = 'POR',
+  kings = 'SAC',
+  spurs = 'SAS',
+  jazz = 'UTA',
+  utb = 'UTB',
+  utw = 'UTW',
+  lebron = 'LBN',
+  hawks = 'ATL',
+  celtics = 'BOS',
+  nets = 'BKN',
+  hornets = 'CHA',
+  bulls = 'CHI',
+  cavaliers = 'CLE',
+  pistons = 'DET',
+  pacers = 'IND',
+  heat = 'MIA',
+  bucks = 'MIL',
+  knicks = 'NYK',
+  magic = 'ORL',
+  sixers = 'PHI',
+  durant = 'DRT',
+  wilbon = 'WLB',
+  stephenA = 'SAS',
+  raptors = 'TOR',
+  usa = 'USA',
+  wizards = 'WAS',
+  world = ' WLD',
+}
+
+export enum TeamId {
+  mavericks = '8',
+  nuggets = '9',
+  warriors = '11',
+  rockets = '14',
+  clippers = '16',
+  lakers = '17',
+  grizzlies = '19',
+  timberwolves = '22',
+  pelicans = '23',
+  thunder = '25',
+  suns = '28',
+  trailBlazers = '29',
+  kings = '30',
+  spurs = '31',
+  lebron = '35',
+  jazz = '40',
+  utb = '102',
+  utw = '103',
+  hawks = '1',
+  celtics = '2',
+  nets = '4',
+  hornets = '5',
+  bulls = "6'",
+  cavaliers = '7',
+  pistons = '10',
+  pacers = '15',
+  heat = '20',
+  bucks = '21',
+  knicks = '24',
+  magic = '26',
+  sixers = '27',
+  durant = '34',
+  away = '36',
+  home = '37',
+  raptors = '38',
+  usa = '39',
+  wizards = '41',
+  world = '42',
+}
+
+export enum City {
+  mavericks = 'Dallas',
+  nuggets = 'Denver',
+  warriors = 'Golden State',
+  rockets = 'Houston',
+  clippers = 'LA',
+  lakers = 'Los Angeles',
+  grizzlies = 'Memphis',
+  timberwolves = 'Minnesota',
+  pelicans = 'New Orleans',
+  thunder = 'Oklahoma City',
+  suns = 'Phoenix',
+  trailBlazers = 'Portland',
+  kings = 'Sacramento',
+  spurs = 'San Antonio',
+  lebron = 'Team',
+  jazz = 'Utah',
+  utb = 'Utah',
+  utw = 'Utah',
+  hawks = 'Atlanta',
+  celtics = 'Boston',
+  nets = 'Brooklyn',
+  hornets = 'Charlotte',
+  bulls = 'Chicago',
+  cavaliers = 'Cleveland',
+  pistons = 'Detroit',
+  pacers = 'Indiana',
+  heat = 'Miami',
+  bucks = 'Milwaukee',
+  knicks = 'New York',
+  magic = 'Orlando',
+  sixers = 'Philadelphia',
+  durant = 'Team',
+  away = 'Away',
+  home = 'Home',
+  raptors = 'Toronto',
+  usa = 'USA',
+  wizards = 'Washington',
+  world = 'World',
 }
 
 export interface Base {
@@ -42,7 +216,27 @@ export interface TeamsData {
   allStar: string
   nbaFranchise: string
   leagues: {
-    standard: {
+    standard?: {
+      confName: Conference
+      divName: Division
+    }
+    vegas?: {
+      confName: Conference
+      divName: Division
+    }
+    sacramento?: {
+      confName: Conference
+      divName: Division
+    }
+    utah?: {
+      confName: Conference
+      divName: Division
+    }
+    orlando?: {
+      confName: Conference
+      divName: Division
+    }
+    africa?: {
       confName: Conference
       divName: Division
     }
@@ -119,7 +313,6 @@ export async function getSeasons(): Promise<Season> {
   const { data } = await nbaApiRequest({ url: '/seasons/' })
   return data
 }
-
 /**
  * The GET /seasons call allows your platform to receive an object containing all available leagues.
  * @summary Get all available leagues
@@ -129,50 +322,77 @@ export async function getLeagues(): Promise<League> {
   const { data } = await nbaApiRequest({ url: '/leagues/' })
   return data
 }
-
+/**
+ * The GET /teams/divName/{divisionName} call allows your platform to receive an object containing all available teams by division.
+ * @summary Get teams by division name
+ * @throws {AxiosError}
+ */
 export async function getTeamsByDivName(divisionName: Division): Promise<Teams> {
   const { data } = await nbaApiRequest({ url: `/teams/divName/${divisionName}` })
   return data
 }
-
+/**
+ * The GET /teams/divName/{conferenceName} call allows your platform to receive an object containing all available teams by division.
+ * @summary Get teams by conference name
+ * @throws {AxiosError}
+ */
 export async function getTeamsByConfName(conferenceName: Conference): Promise<Teams> {
   const { data } = await nbaApiRequest({ url: `/teams/confName/${conferenceName}` })
   return data
 }
-
-export async function getTeamsByNickName(nickName: string): Promise<Teams> {
+/**
+ * The GET /teams/divName/{nickName} call allows your platform to receive an object containing all available teams by division.
+ * @summary Get teams by nickname
+ * @throws {AxiosError}
+ */
+export async function getTeamsByNickName(nickName: NickName): Promise<Teams> {
   const { data } = await nbaApiRequest({ url: `/teams/nickName/${nickName}` })
   return data
 }
-
-export async function getTeamsByShortName(shortName: string): Promise<Teams> {
+/**
+ * The GET /teams/divName/{shortName} call allows your platform to receive an object containing all available teams by division.
+ * @summary Get teams by shortName
+ * @throws {AxiosError}
+ */
+export async function getTeamsByShortName(shortName: ShortName): Promise<Teams> {
   const { data } = await nbaApiRequest({ url: `/teams/shortName/${shortName}` })
   return data
 }
-
-export async function getTeamsByCity(city: string): Promise<Teams> {
+/**
+ * The GET /teams/divName/{city} call allows your platform to receive an object containing all available teams by division.
+ * @summary Get teams by city
+ * @throws {AxiosError}
+ */
+export async function getTeamsByCity(city: City): Promise<Teams> {
   const { data } = await nbaApiRequest({ url: `/teams/city/${city}` })
   return data
 }
-
-export async function getTeamsByLeague(league: string): Promise<Teams> {
+/**
+ * The GET /teams/divName/{league} call allows your platform to receive an object containing all available teams by division.
+ * @summary Get teams by league
+ * @throws {AxiosError}
+ */
+export async function getTeamsByLeague(league: League): Promise<Teams> {
   const { data } = await nbaApiRequest({ url: `/teams/league/${league}` })
   return data
 }
-
-export async function getTeamsById(id: string): Promise<Teams> {
+/**
+ * The GET /teams/divName/{id} call allows your platform to receive an object containing all available teams by division.
+ * @summary Get teams by id
+ * @throws {AxiosError}
+ */
+export async function getTeamsById(id: TeamId): Promise<Teams> {
   const { data } = await nbaApiRequest({ url: `/teams/teamId/${id}` })
   return data
 }
-
 /**
  * The GET /standings/{league}/{seasonYear} call allows your platform to receive an object containing a list of the Team standings for your provided year.
  * @summary Get standings for a certain year
- * @param {string} seasonYear
+ * @param {Year} Year
  * @param {Leagues} Leagues
  * @throws {AxiosError}
  */
-export async function getStandings(seasonYear: string, league: Leagues): Promise<Standings> {
+export async function getStandings(seasonYear: Year, league: Leagues): Promise<Standings> {
   const { data } = await nbaApiRequest({ url: `/standings/${league}/${seasonYear}/` })
   return data
 }
